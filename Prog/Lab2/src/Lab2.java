9d996f0837690988cbeb74fc6e06dfecc29e3881
felijy <kulaginvyacheslaw@gmail.com>
2023-10-21 00:54:17 +0300

    rewrite Lab2

--- Prog/Lab2/src/Lab2.java
+++ Prog/Lab2/src/Lab2.java
@@ -6,11 +6,11 @@ public class Lab2 {
     public static void main(String[] args) {
         Battle b = new Battle();
         Throh r1 = new Throh("ниндзя", 5);
-        Slowpoke g1 = new Slowpoke("слоупок", 6);
-        Slowking r2 = new Slowking("ребенок слоупока", 12);
-        Bounsweet g2 = new Bounsweet("трава", 20);
-        Steenee r3 = new Steenee("трава X", 34);
-        Tsareena g3 = new Tsareena("трава XX", 22);
+        Slowpoke g1 = new Slowpoke("слоупок", 5);
+        Slowking r2 = new Slowking("ребенок слоупока", 5);
+        Bounsweet g2 = new Bounsweet("трава", 5);
+        Steenee r3 = new Steenee("трава X", 5);
+        Tsareena g3 = new Tsareena("трава XX", 5);
         b.addFoe(r1);
         b.addFoe(r2);
         b.addFoe(r3);
